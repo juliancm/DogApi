6c33a859f37724ad201868a4fceded3fb362123c
juliancm <jc.juliancastro@gmail.com>
2020-02-19 21:10:03 -0600

    ready for q.c.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-function constDropdown() {
+'use strict'
+
+function constConsoleDropdown() {
 
   let dropdownString ="";
 
   dropdownString += `
     <form>
       <fieldset>
-        <label for="dogs">How many dogs?</label>
-          <select id="dogs" name="dogs">
+        <label for="consoleDogs">How many dogs?</label>
+          <select id="consoleDogs" name="dogs">
   `
 
   for (let i=1;i<3;i++) {
@@ -21,7 +23,7 @@ function constDropdown() {
 
   dropdownString += `
           </select>
-        <button type="submit" class="submitAnswer"> Submit</button>
+        <button type="submit" class="postConsole">Console</button>
       </fieldset>
     </form>
   `
@@ -29,19 +31,71 @@ function constDropdown() {
   $('.consolePostMultiRand').html(dropdownString);
 }
 
-function getMultiDogJson() {
-  let selected = $('#dogs option:selected').val();
+function consoleMultiDogJson() {
+  let selected = $('#consoleDogs option:selected').val();
   fetch(`https://dog.ceo/api/breeds/image/random/${selected}`)
     .then(response => response.json())
     .then(responseJson =>
-      displayResults(responseJson))
-      /*or console.log(responseJson)*/
+      console.log(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 
 }
 
+function submitConsolePostMultiRandClick () {
+
+    $('.postConsole').on('click', function(event) {
+        event.preventDefault();
+        consoleMultiDogJson();
+    });
+}
+
+/*fuse dom and console into single set of functions?*/
+
+function constDomDropdown() {
+
+  let dropdownString ="";
+
+  dropdownString += `
+    <form>
+      <fieldset>
+        <label for="displayDogs">How many dogs?</label>
+          <select id="displayDogs" name="dogs">
+  `
+
+  for (let i=1;i<3;i++) {
+    dropdownString += `<option value="${i}">${i}</option>`
+  };
+
+  dropdownString += `<option value="3" selected>3</option>`
+
+  for (let i=4;i<51;i++) {
+    dropdownString += `<option value="${i}">${i}</option>`
+  };
+
+  dropdownString += `
+          </select>
+        <button type="submit" class="displayDom">Display</button>
+      </fieldset>
+    </form>
+    <div class='multiDogDisplayArea'></div>
+  `
+
+  $('.displayDomMultiRand').html(dropdownString);
+}
+
+function displayMultiDogJson() {
+  let selected = $('#displayDogs option:selected').val();
+  fetch(`https://dog.ceo/api/breeds/image/random/${selected}`)
+    .then(response => response.json())
+    .then(responseJson =>
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+
+}
 
 function displayResults(responseJson) {
+
+
 
 let imgString = ""
 
@@ -49,91 +103,113 @@ for (let i=0;i<responseJson.message.length;i++) {
   imgString += `<img src="${responseJson.message[i]}" class="results-img">`
 };
 
-$('.displayDomMultiRand').html(imgString);
+$('.multiDogDisplayArea').html(imgString);
 
 }
 
+function submitDisplayMultiRandClick () {
 
-function submitConsolePostMultiRandClick () {
+    $('.displayDom').on('click', function(event) {
+        event.preventDefault();
+        displayMultiDogJson();
+    });
+}
 
-    $('.submitAnswer').on('click', function(event) {
+/*follows workspace for randBreed*/
+
+function constRandByBreedInput () {
+
+  let inputString ="";
+
+  inputString += `
+    <form>
+      <fieldset>
+      <label for="breedInput">Breed:</label>
+        <input type="text" id="breedInput">
+        <button type="submit" class="displayBreed">Display</button>
+      </fieldset>
+    </form>
+    <div class='singleBreedDisplayArea'></div>
+  `
+
+  $('.displayDomSingleBreed').html(inputString);
+}
+
+function submitSingleBreedInputClick () {
+
+    $('.displayBreed').on('click', function(event) {
         event.preventDefault();
 
-        if ($('#dogs option:selected').val() === '45') {
+        if ($('#breedInput').val() === '') {
 
 /* Strict equality: 45 the string is not the 45 integer*/
 
-            alert('butt');
+            alert('please input a breed');
 
         } else {
-            getMultiDogJson();
+            getSingleBreedJson();
+            normalizeSingleBreedInput();
         }
     });
 }
 
-constDropdown();
+function getSingleBreedJson() {
+  let selected = $('#breedInput').val();
+  if (selected.includes(' ')) {
+    selected = normalizeSingleBreedInput();
+  }
+  fetch(`https://dog.ceo/api/breed/${selected}/images/random`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson =>
+      displayBreedResults(responseJson))
+    .catch(error => {
+      $('.singleBreedDisplayArea').html(`<p>${error.message}</p>`);
+    });
+
+}
+
+function normalizeSingleBreedInput () {
+  let str = $('#breedInput').val();
+  let strArray = str.split(' ');
+  let newStr = strArray[1]+' '+strArray[0];
+  return newStr.replace(/ /g, '/');
+}
+
+/* function breedCheck () {
+  $.getJSON('https://dog.ceo/api/breeds/list/all', function (result) {
+    console.log(result);
+  });
+} */
+
+
+function breedError (responseJson) {
+  let errorString = `<p>${responseJson.message}</p>`
+
+  $('.singleBreedDisplayArea').html(errorString);
+}
+
+function displayBreedResults(responseJson) {
+
+
+let imgString = ""
+
+imgString += `<img src="${responseJson.message}" class="results-img">`
+
+$('.singleBreedDisplayArea').html(imgString);
+
+}
+
+
+constConsoleDropdown ();
 submitConsolePostMultiRandClick ();
 
-/*function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random/3')
-    .then(response => response.json())
-    .then(responseJson =>
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
-}
+constDomDropdown ();
+submitDisplayMultiRandClick ();
 
-function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  $('.chickentest').append(
-    `<img src="${responseJson.message[1]}" class="results-img">`
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
-}
-
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    getMoreDogImage();
-  });
-}
-/*currently testing with default of 3 add selection input*/
-/*function dogNumber () {
-  return 3;
-}
-
-function createMultipleEndpoint () {
-  return `'https://dog.ceo/api/breeds/image/random/${dogNumber()}'`;
-}
-
-function getMoreDogImage () {
-  /*take the user input and create a new endpoint url*/
-/*  fetch(createMultipleEndpoint())
-  .then(response => response.json())
-  .then(responseJson => displayResults(reponseJson))
-  .catch(error => alert('Something went wrong. Try again later.'));
-}
-
-$(function() {
-  console.log('App loaded! Waiting for submit!');
-  watchForm();
-}); */
-
-/* input from jquery for a specific input
-name attribute $('input[name=optionChoice]:checked').val() */
-
-
-/*      <fieldset>
-        <label for="dogs">How many dogs?</label>
-        <select id="dogs">
-          <option value="1">1</option>
-      </fieldset>
-*/
-
-/*https://dog.ceo/api/breeds/image/random/3*/
-
-/* to create drop down menu use js to iterate each line element*/
-
-
-/*display by breed https://dog.ceo/api/breed/hound/images*/
+constRandByBreedInput ();
+submitSingleBreedInputClick ();
